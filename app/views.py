adf80e3ec8852e6b377942c436990471d3802c61
FloKots <florencekotohoyoh@gmail.com>
2022-05-02 20:47:50 +0300

    get top-headlines news

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -7,8 +8,12 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
+
+  # Getting Top Headlines News
+  top_headlines = get_news('top-headlines')
+  print(top_headlines)
   title = "The Nightngale News App"
-  return render_template('index.html', title = title)
+  return render_template('index.html', title = title, top_headlines = top_headlines)
 
 
 @app.route('/news/<id>')
