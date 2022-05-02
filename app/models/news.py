7de874ef754fca7edfa347a0f5bbc55756c6a6fe
FloKots <florencekotohoyoh@gmail.com>
2022-05-02 15:32:21 +0300

    process results

--- app/models/news.py
+++ app/models/news.py
@@ -6,13 +6,12 @@ class News:
   News class to define News Objects
   '''
 
-  def __init__(self, id, name, author, title, description, url, urlToImage, time):
+  def __init__(self, id, name, description, url, category, language, country):
     
     self.id = id 
     self.name = name
-    self.author = author
-    self.title = title
     self.description = description
-    self.url = url
-    self.urlToImage = urlToImage
-    self.time = time
+    self.url= url
+    self.category = category
+    self.language = language
+    self.country = country
--- app/news_test.py
+++ app/news_test.py
@@ -12,8 +12,7 @@ class NewsTest(unittest.TestCase):
     Set up method that will run before every Test
     '''
     
-    self.new_news = News(1234, "Flask", "Flask Author", "The Evolution of Flask", "The Story of how Flask Evolved", "https://www.spiegel.de/wirtschaft/unternehmen/adler-group-aktie-von-immobilienkonzern-stuerzt-ab-pruefer-verweigern-testat-a-1f3d36b6-3772-44d8-b5c7-87eaedd4a301", "https://cdn.prod.www.spiegel.de/images/5a0e3272-55fa-43b3-b2bd-93a14b246bc8_w1280_r1.77_fpx50.67_fpy50.jpg", "2022-05-02T08:48:58Z")
-
+    self.new_news = News("abc-news", "ABC News", "Your trusted source for breaking news, analysis, exclusive interviews, headlines, and videos at ABCNews.com.", "https://abcnews.go.com", "general", "en", "us")
   
   def test_instance(self):
     self.assertTrue(isinstance(self.new_news, News))
